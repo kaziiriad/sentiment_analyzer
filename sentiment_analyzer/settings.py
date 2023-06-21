d74225b9d489398046e1602d709dd1c0ff164366
Sultan Mahmud <kaziiriad@gmail.com>
2023-06-21 21:54:23 +0600

    modified settings.py

--- sentiment_analyzer/settings.py
+++ sentiment_analyzer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'sentiment_analyzer.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    
+
     'default': {
 
         'ENGINE': 'django.db.backends.sqlite3',
