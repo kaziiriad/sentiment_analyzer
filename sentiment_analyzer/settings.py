955816b1b8962855ad330d8030106825d8b44199
Sultan Mahmud <kaziiriad@gmail.com>
2023-06-21 21:02:07 +0600

    added .gitignore

--- sentiment_analyzer/settings.py
+++ sentiment_analyzer/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@ut5r%7+%(&bplenfx&l&(5*aj5l#h=du!8xzcw#u^rh$hm7u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
